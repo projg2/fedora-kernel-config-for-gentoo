321d4e224b9db546dc57977d47126f26e01a4369
Justin M. Forbes <jforbes@redhat.com>
2016-10-20 09:00:59 -0500

    Linux v4.7.9 fixes CVE-2016-5195

--- scripts/generate-git-snapshot.sh
+++ scripts/generate-git-snapshot.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
+# This script allows for the generation of a git snapshot between the upstream
+# git tree and the current tree.
 #
-# Set LINUX_GIT to point to an upstream Linux git tree in your .bashrc or wherever.
+# Prerequisites:
+#     Set LINUX_GIT to point to an upstream Linux git tree in your .bashrc
+#     or wherever.
 
-[ ! -d "$LINUX_GIT" ] && echo "error: set \$LINUX_GIT to point at upstream git tree" && exit 1
+# Look to see if LINUX_GIT is set in local .bashrc
+if [ -f ~/.bashrc  ]; then
+    source ~/.bashrc
+fi
+
+if [ ! -d "$LINUX_GIT" ]; then
+    echo "error: set \$LINUX_GIT to point at upstream git tree"
+    exit 1
+fi
 
 VER=$(grep patch sources | head -n1 | awk '{ print $2 }' | sed s/patch-// | sed s/-git.*// | sed s/.xz//)
+
+if [ -z "$VER" ] ;
+then
+    VER=$(grep linux sources | head -1 | awk '{ print $2 }' | sed s/linux-// | sed s/.tar.xz//)
+fi
 
 OLDGIT=$(grep gitrev kernel.spec | head -n1 | sed s/%define\ gitrev\ //)
 export NEWGIT=$(($OLDGIT+1))
@@ -14,9 +31,11 @@ pushd $LINUX_GIT
 git diff v$VER.. > /tmp/patch-$VER-git$NEWGIT
 xz -9 /tmp/patch-$VER-git$NEWGIT
 DESC=$(git describe)
+git rev-list --max-count=1 HEAD > /tmp/gitrev
 popd
 
 mv /tmp/patch-$VER-git$NEWGIT.xz .
+mv /tmp/gitrev .
 
 perl -p -i -e 's|%global baserelease.*|%global baserelease 0|' kernel.spec
 
